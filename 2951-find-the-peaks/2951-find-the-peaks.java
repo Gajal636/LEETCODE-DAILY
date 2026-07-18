6f030f2484099a825a8f7a5ff752ab0b4efd5d59
Gajal Gupta <159435865+Gajal636@users.noreply.github.com>
2026-07-18 14:34:03 +0530

    Time: 1 ms (99.89%), Space: 45.4 MB (53.39%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.java
+++ 2951-find-the-peaks/2951-find-the-peaks.java
@@ -3,7 +3,7 @@ class Solution {
         List<Integer> ans=new ArrayList<>();
 
       for(int i=1;i<mountain.length-1;i++){
-        if(mountain[i]>mountain[i+1] && mountain[i]>mountain[i-1] ){
+        if(mountain[i]>mountain[i-1] && mountain[i]>mountain[i+1] ){
             ans.add(i);
         }
       }
