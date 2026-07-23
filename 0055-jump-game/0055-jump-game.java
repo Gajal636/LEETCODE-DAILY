7ce142406226b5a908fa266e68f75eb7f6eb70fb
Gajal Gupta <159435865+Gajal636@users.noreply.github.com>
2026-07-23 16:02:25 +0530

    Time: 1 ms (99.94%), Space: 47.9 MB (62.85%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,27 +1,11 @@
 class Solution {
-    static boolean solve(int nums[],int i,Boolean[]dp){
-        if(i==nums.length-1){
-            return true;
-        }if(i>=nums.length){
-            return false;
-        }if(nums[i]==0){
-            return false;
-        }if(dp[i]!=null){
-            return dp[i];
-        }
-        int jumpVal=nums[i];
-     
-        for(int jump=1;jump<=jumpVal;jump++){
-           if(solve(nums,i+jump,dp)){
-            return dp[i]=true;
-           }
-        }
-     
-        return dp[i]=false;
-    }
     public boolean canJump(int[] nums) {
-        Boolean[] dp=new Boolean[nums.length];
-      
-        return solve(nums,0,dp);
+        int goal=nums.length-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(i+nums[i]>=goal){
+                goal=i;
+            }
+        }
+        return goal==0;
     }
 }
