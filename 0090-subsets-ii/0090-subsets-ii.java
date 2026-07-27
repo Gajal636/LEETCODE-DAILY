3cb26f4d6f01bb1a99bc404af133c2513e31bfc6
Gajal Gupta <159435865+Gajal636@users.noreply.github.com>
2026-07-27 12:57:30 +0530

    Time: 2 ms (99.7%), Space: 45.3 MB (24.53%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    static void solve(int nums[],int index,List<Integer> output,List<List<Integer>> ans){
+        if(index>=nums.length){
+        ans.add(new ArrayList<>(output));
+        return;
+    }
+    int currValue=nums[index];
+    output.add(currValue);
+    
+    solve(nums,index+1,output,ans);
+    output.remove(output.size()-1);
+    while(index+1<nums.length && nums[index]==nums[index+1]){
+        index++;
+    }
+
+    solve(nums,index+1,output,ans);
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+         List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> output=new ArrayList<>();
+        int index=0;
+         solve(nums,index,output,ans);
+         return ans;
+    }
+}
