d816f1c7bcb0b969dcbb225549649d9bc4e30bc5
Gajal Gupta <159435865+Gajal636@users.noreply.github.com>
2026-07-29 20:37:11 +0530

    Time: 1 ms (98.38%), Space: 44.4 MB (84.97%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.java
+++ 3046-split-the-array/3046-split-the-array.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isPossibleToSplit(int[] nums) {
        int[] freq = new int[101];
-       for(int num=0;num<nums.length;num++){
-        freq[nums[num]]++;
-        if(freq[nums[num]]>2){
+       for(int num:nums){
+        freq[num]++;
+        if(freq[num]>2){
             return false;
         }
        }
