0a794ad75f58b53795fcd2f6b187a494ba4e175e
Gajal Gupta <159435865+Gajal636@users.noreply.github.com>
2026-08-01 21:44:12 +0530

    Time: 0 ms (100%), Space: 42.7 MB (57.54%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,20 @@
+class Solution {
+    static int solve(int nums[],int i,int j, Integer [][] dp){
+        if(i==j){
+            return nums[i];
+        }if(dp[i][j]!=null){
+            return dp[i][j];
+        }
+
+        int playerOne=nums[i]-solve(nums,i+1,j,dp);
+        int playerTwo=nums[j]-solve(nums,i,j-1,dp);
+
+        return dp[i][j]=Math.max(playerOne,playerTwo);
+    }
+    public boolean predictTheWinner(int[] nums) {
+        int n=nums.length;
+        Integer[][] dp=new Integer[n][n];
+        return solve(nums,0,n-1,dp) >=0;
+        
+    }
+}
