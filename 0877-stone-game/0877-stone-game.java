925fbbe092179fe170ba150889ae4dbb88cca7d6
Gajal Gupta <159435865+Gajal636@users.noreply.github.com>
2026-08-02 11:45:58 +0530

    Time: 0 ms (100%), Space: 43 MB (62.03%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,19 +1,7 @@
 class Solution {
-    static int solve(int[]piles,int i,int j,Integer[][]dp){
-        if(i==j){
-            return piles[i];
-        }
-        if(dp[i][j]!=null){
-            return dp[i][j];
-        }
-        int chooseStart=piles[i]-solve(piles,i+1,j,dp);
-        int chooseEnd=piles[j]-solve(piles,i,j-1,dp);
-        dp[i][j]=Math.max(chooseStart,chooseEnd);
-        return dp[i][j];
-    }
+    // static int solve(int[]piles,int i,int j,Integer[][]dp){
+     
     public boolean stoneGame(int[] piles) {
-    int n=piles.length;
-    Integer [][]dp=new Integer[n][n];
-    return solve(piles,0,n-1,dp)>=0;
+    return true;
     }
 }
