27260dc4adb84845993628adc669c77cfed0fc3b
Gajal Gupta <159435865+Gajal636@users.noreply.github.com>
2026-09-13 12:44:38 +0530

    Time: 1 ms (56.17%), Space: 47.5 MB (89.91%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int l=0;
+        int r=n-1;
+        int lMax=0;
+        int rMax=0;
+        int ans=0;
+        while(l<r){
+            lMax=Math.max(lMax,height[l]);
+            rMax=Math.max(rMax,height[r]);
+
+            if(lMax<rMax){
+                ans+=lMax-height[l];
+                l++;
+            }else{
+                ans+=rMax-height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+}
